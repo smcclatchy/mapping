4b37493b20c50019280a57241a03a4344196097f
Sue McClatchy <susan.mcclatchy@jax.org>
2017-12-20 09:50:39 -0500

    updated figures

--- fig/marker-regress.R
+++ fig/marker-regress.R
@@ -2,10 +2,10 @@ x <- pull.geno(hyper)[,"D4Mit214"]
 u <- runif(length(x), -0.075, 0.075)
 y <- hyper$pheno[,1]
 
-plot(y ~ x, type="n", xlab="Genotype", ylab="Phenotype",
-     xlim=c(0.5,2.5), xaxs="i", xaxt="n")
+plot(y ~ x, type="n", xlab="Genotype", ylab="Blood Pressure",
+     xlim=c(0.5,2.5), xaxs="i", xaxt="n", main = "Marker Regression")
 abline(v=1:2, lty=2, col="gray50")
-points(x+u, y)
+points(x + 0.3 * u, y)
 axis(side=1, at=1:2, labels=c("AA","AB"))
 me <- tapply(y, x, mean)
 segments((1:2)-0.15, me, (1:2)+0.15, me, lwd=3, col="green2")
