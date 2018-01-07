e382d024eeb05e2ff4777aa7eb28b9b4a45bcc4d
Sue McClatchy <susan.mcclatchy@jax.org>
2018-01-06 19:28:31 -0500

    added more equations

--- fig/residual.R
+++ fig/residual.R
@@ -4,7 +4,7 @@ y <- hyper$pheno[,1]
 
 png(filename = "residual.png", width = 600, height = 600)
 plot(y ~ x, type="n", xlab="Genotype", ylab="Blood Pressure",
-     xlim=c(0.5,2.5), xaxs="i", xaxt="n", main = "Line of Best Fit")
+     xlim=c(0, 3), xaxs="i", xaxt="n", main = "Line of Best Fit")
 abline(v=1:2, lty=2, col="gray90")
 points(x + 0.3 * u, y, col = "gray90")
 axis(side=1, at=1:2, labels=c("AA","AB"))
@@ -12,6 +12,6 @@ me <- tapply(y, x, mean)
 segments((1:2)-0.15, me, (1:2)+0.15, me, lwd=3, col="orange1")
 abline(lm(y~x)$coef, col="blue", lwd=2)
 points(1, 119.58, col = "red")
-segments(x0 = 1, y0 = 104.4, y1 = 119, lwd=2, col="red")
+segments(x0 = 1, y0 = 104.4, y1 = 119.5, lwd=2, col="red")
 text(x = 0.8, y = 112, labels = "error or\n residual")
 dev.off()
